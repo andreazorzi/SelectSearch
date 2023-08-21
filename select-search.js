4324c57f1b67aa0079f59603261d6ba6757422ba
Andrea Zorzi <andreaz@europlan.it>
2023-08-21 09:29:41 +0200

    init

---
+++ select-search.js
@@ -0,0 +1,16 @@
+/*!
+ * SelectSearch
+ * https://github.com/andreazorzi/SelectSearch
+ * 
+ * Author: Andrea Zorzi <info@zorziandrea.com>
+ * License: MIT
+ * 
+ * Version: 1.0.0
+ */
+
+import default_lang from "./locale/it.js";
+window.default_lang = default_lang;
+
+export default class SelectSearch{
+	
+}
