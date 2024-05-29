791f066ab56c4c7639bc2aad977f9dd1bc4174cd
Andrea Zorzi <andreaz@europlan.it>
2024-05-29 09:33:12 +0200

    Fix class override problem

--- select-search.js
+++ select-search.js
@@ -22,9 +22,9 @@ export default class SelectSearch{
         display_empty: false,
         always_display_empty: false,
         custom_class: {
-            placeholder: "select-search-placeholder",
-            search_input: "select-search-input",
-            list_item: "select-search-item"
+            placeholder: "select-search-placeholder-style",
+            search_input: "select-search-input-style",
+            list_item: "select-search-item-style"
         },
         render: (element) => {
             return element.textContent
@@ -43,7 +43,11 @@ export default class SelectSearch{
         }
         
         this.#element = element;
+        let classes = { ...this.#options.custom_class, ...options.custom_class };
         this.#options = { ...this.#options, ...options };
+        this.#options.custom_class = classes;
+        
+        console.log(this.#options.custom_class);
         
         this.#init();
     }
@@ -108,9 +112,9 @@ export default class SelectSearch{
     
     #getModal(){
         return `
-            <div type="text" class="${this.#options.custom_class.placeholder}"></div>
+            <div type="text" class="select-search-placeholder ${this.#options.custom_class.placeholder}"></div>
             <div class="select-search-modal" style="display: none;">
-                <input type="text" class="${this.#options.custom_class.search_input}" placeholder="${this.#options.lang.search}...">
+                <input type="text" class="select-search-input ${this.#options.custom_class.search_input}" placeholder="${this.#options.lang.search}...">
                 <div class="select-search-list"></div>
             </div>
         `;
@@ -174,7 +178,7 @@ export default class SelectSearch{
                 
                 if((item.text.toLowerCase().includes(query.toLowerCase()) && (counter < this.#options.list_limit || this.#options.list_limit == -1)) || (this.#options.always_display_empty && item.value == "")){
                     this.#container.querySelector(".select-search-list").insertAdjacentHTML("beforeend", `
-                        <div class="${this.#options.custom_class.list_item} ${this.#checkSelected(item.value) ? "selected" : ""}" data-value="${item.value}">
+                        <div class="select-search-item ${this.#options.custom_class.list_item} ${this.#checkSelected(item.value) ? "selected" : ""}" data-value="${item.value}">
                             ${item.html}
                         </div>
                     `);
